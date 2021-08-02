2cb6a5fad1ab4774c3860f051ece452abc7c5b6d
deepcloudlabs <deepcloudlabs@gmail.com>
2021-08-02 15:37:04 +0300

    initial commit

--- lottery/src/component/lottery.js
+++ lottery/src/component/lottery.js
@@ -8,9 +8,42 @@ class Lottery extends React.PureComponent {
             numbers: [],
             column: 0
         }
+        // this.get_lottery_numbers = this.get_lottery_numbers.bind(this);
     }
 
-    render(){
+    draw = () => {
+        let lottery_numbers = this.get_lottery_numbers() ;
+        let new_numbers= [...this.state.numbers] // cloning the array
+        new_numbers.push(lottery_numbers)
+        this.setState({ // async!
+            numbers: new_numbers
+        },() => {
+            console.log("callback: "+this.state.numbers)
+        })
+        console.log("after setState: "+this.state.numbers)
+    }
+
+    reset = () => {
+        this.setState({ // asynchronous function
+            numbers: []
+        }, () => {
+            console.log("callback: "+this.state.numbers)
+        })
+
+    }
+
+    get_lottery_numbers = () => {
+        let numbers = []
+        while(numbers.length < 6){
+            let number = Math.floor(Math.random()*60)+1
+            if(!numbers.includes(number))
+                numbers.push(number)
+        }
+        numbers.sort((x,y) => x-y)
+        return numbers
+    }
+
+    render = () => {
        return (
           <div className="container">
               <div className="card">
@@ -24,11 +57,40 @@ class Lottery extends React.PureComponent {
                                   className="form-control"></input>
                        </div>
                        <div className="form-group">
-                           <button className="btn btn-success">Draw</button>
-                           <button className="btn btn-danger">Reset</button>
+                           <button onClick={this.draw}
+                               className="btn btn-success">Draw</button>
+                           <button onClick={this.reset}
+                                   className="btn btn-danger">Reset</button>
                        </div>
                   </div>
-
+              </div>
+              <p></p>
+              <div className="card">
+                  <div className="card-header">
+                      <div className="card-title"><h3>Lottery Numbers</h3></div>
+                  </div>
+                  <div className="card-body">
+                      <table className="table table-responsive table-striped table-hover table-bordered">
+                          <thead>
+                            <tr>
+                                {
+                                    Array.from(Array(6).keys()).map( i =>
+                                        <th key={i}>Column #{i+1}</th>
+                                    )
+                                }
+                            </tr>
+                          </thead>
+                          <tbody>{
+                             this.state.numbers.map( nums =>
+                                 <tr>{
+                                    nums.map( number =>
+                                        <td key={number}>{number}</td>
+                                    )
+                                 }</tr>
+                             )
+                          }</tbody>
+                      </table>
+                  </div>
               </div>
           </div>
        ) ;
