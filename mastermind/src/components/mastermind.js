89f2c66faa0f9ea155c3645b302b8f82ce4aa8c4
deepcloudlabs <deepcloudlabs@gmail.com>
2021-08-03 11:08:05 +0300

    add function components

--- mastermind/src/components/mastermind.js
+++ mastermind/src/components/mastermind.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GameStatistics from "./statistics";
 import CardTitle from "./card_title";
+import Badge from "./badge";
 
 export default class Mastermind extends React.PureComponent {
     constructor(props,context) {
@@ -8,7 +9,7 @@ export default class Mastermind extends React.PureComponent {
         this.state = {
             gameLevel : 3,
             tries: 0,
-            secret: 549,
+            secret: this.createSecret(3),
             moves: [],
             guess : 123,
             statistics: {
@@ -16,6 +17,30 @@ export default class Mastermind extends React.PureComponent {
                 loses: 5
             }
         }
+    }
+
+    createRandomDigit = (min,max) => {
+        return Math.floor(Math.random()*(max-min+1))+min;
+    }
+
+    createSecret = (level) => {
+        let digits = [ this.createRandomDigit(1,9)];
+        while (digits.length < level){
+            let digit = this.createRandomDigit(0,9);
+            if (!digits.includes(digit))
+               digits.push(digit);
+        }
+        return digits.reduce((number,digit) => 10 * number + digit  , 0);
+    }
+
+    handleInputGuess = (event) => {
+        this.setState({
+            "guess": Number(event.target.value)
+        })
+    }
+
+    play = () => {
+
     }
 
     render = () => {
@@ -26,7 +51,21 @@ export default class Mastermind extends React.PureComponent {
                     <CardTitle title="Game Console" />
                   </div>
                   <div className="card-body">
-
+                      <Badge label="Game Level" value={this.state.gameLevel} />
+                      <Badge label="Tries" value={this.state.tries} />
+                      <Badge label="Secret" value={this.state.secret} />
+                      <div className="form-group">
+                          <label htmlFor="guess">Guess: </label>
+                          <input type="number"
+                                 id="guess"
+                                 value={this.state.guess}
+                                 onChange={this.handleInputGuess}
+                                 className="form-control"></input>
+                      </div>
+                      <div className="form-group">
+                          <button  onClick={this.play}
+                                   className="btn btn-success">Play</button>
+                      </div>
                   </div>
               </div>
               <p></p>
