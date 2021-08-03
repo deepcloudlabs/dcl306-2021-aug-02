b76b4f063b2c5250ee9d07da1a29f64af1329be0
deepcloudlabs <deepcloudlabs@gmail.com>
2021-08-03 16:06:53 +0300

    initial commit

--- hr-frontend/src/App.js
+++ hr-frontend/src/App.js
@@ -3,6 +3,29 @@ import Employee from "./model/employee";
 
 function HrApp() {
     let [employee, setEmployee] = useState(new Employee());
+
+    function handleInputChange(event){
+        const value = event.target.value;
+        const name = event.target.name;
+        let emp = {...employee} ; //cloning employee
+        if (name === "fulltime"){
+            emp.fulltime = !emp.fulltime;
+        } else {
+            emp[name] = value;
+        }
+        setEmployee(emp);
+    }
+
+    function handleFileInput(event){
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onload = (e) => {
+            let emp = {...employee} ; //cloning employee
+            emp.photo = e.target.result;
+            setEmployee(emp)
+        }
+    }
+
     return (
         <div className="container">
             <div className="card">
@@ -16,6 +39,7 @@ function HrApp() {
                                className="form-control"
                                id="identityNo"
                                name="identityNo"
+                               onChange={handleInputChange}
                                value={employee.identityNo}></input>
                     </div>
                     <div className="form-group">
@@ -24,6 +48,7 @@ function HrApp() {
                                className="form-control"
                                id="fullname"
                                name="fullname"
+                               onChange={handleInputChange}
                                value={employee.fullname}></input>
                     </div>
                     <div className="form-group">
@@ -32,6 +57,7 @@ function HrApp() {
                                className="form-control"
                                id="iban"
                                name="iban"
+                               onChange={handleInputChange}
                                value={employee.iban}></input>
                     </div>
                     <div className="form-group">
@@ -40,6 +66,7 @@ function HrApp() {
                                className="form-control"
                                id="salary"
                                name="salary"
+                               onChange={handleInputChange}
                                value={employee.salary}></input>
                     </div>
                     <div className="form-group">
@@ -48,6 +75,7 @@ function HrApp() {
                                className="form-control"
                                id="birthYear"
                                name="birthYear"
+                               onChange={handleInputChange}
                                value={employee.birthYear}></input>
                     </div>
                     <div className="form-group">
@@ -55,6 +83,7 @@ function HrApp() {
                         <select className="form-control"
                                id="department"
                                name="department"
+                               onChange={handleInputChange}
                                value={employee.department}>
                             <option>IT</option>
                             <option>Sales</option>
@@ -66,13 +95,19 @@ function HrApp() {
                         <label htmlFor="photo">Photo:</label>
                         <img id="photo" src={employee.photo} />
                         <label className="btn btn-success">
-                            <input type="file" style={{display: "none"}}></input>
+                            <input type="file"
+                                   onChange={handleFileInput}
+                                   style={{display: "none"}}></input>
                             Load
                         </label>
                     </div>
                     <div className="form-group">
                         <label htmlFor="fulltime">Fulltime?:</label>
-                        <input type="checkbox" id="fulltime" name="fulltime" />
+                        <input type="checkbox"
+                               id="fulltime"
+                               checked={employee.fulltime}
+                               onChange={handleInputChange}
+                               name="fulltime" />
                     </div>
                 </div>
             </div>
