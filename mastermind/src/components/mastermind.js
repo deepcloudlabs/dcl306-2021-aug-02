1be4311885a3d0049c2ad06f83f26ca1b5172af0
deepcloudlabs <deepcloudlabs@gmail.com>
2021-08-04 12:23:41 +0300

    convert Mastermind to redux

--- mastermind/src/components/mastermind.js
+++ mastermind/src/components/mastermind.js
@@ -4,22 +4,10 @@ import CardTitle from "./card_title";
 import Badge from "./badge";
 import Move from "../model/move";
 
-export default class Mastermind extends React.PureComponent {
-    constructor(props,context) {
-        super(props,context);
-        this.state = {
-            gameLevel : 3,
-            tries: 0,
-            secret: this.createSecret(3),
-            moves: [],
-            guess : 123,
-            statistics: {
-                wins: 0,
-                loses: 0
-            }
-        }
-    }
+// stateful -> stateless
+export default function Mastermind(props) {
 
+    /*
     componentDidMount() {
         let gameState = localStorage.getItem("mastermind-state")
         if (gameState === null || gameState === undefined){
@@ -30,118 +18,10 @@ export default class Mastermind extends React.PureComponent {
             this.setState(game);
         }
     }
+     */
 
-    createRandomDigit = (min,max) => {
-        return Math.floor(Math.random()*(max-min+1))+min;
-    }
-
-    createSecret = (level) => {
-        let digits = [ this.createRandomDigit(1,9)];
-        while (digits.length < level){
-            let digit = this.createRandomDigit(0,9);
-            if (!digits.includes(digit))
-               digits.push(digit);
-        }
-        return digits.reduce((number,digit) => 10 * number + digit  , 0);
-    }
-
-    handleInputGuess = (event) => {
-        this.setState({
-            "guess": Number(event.target.value)
-        })
-    }
-
-    evaluateMove = (secret,guess) => {
-        let secretAsString = secret.toString();
-        let guessAsString = guess.toString();
-        let perfectMatch = 0;
-        let partialMatch = 0;
-        for (let i=0;i<secretAsString.length;i++){
-            let s = secretAsString.charAt(i);
-            for (let j=0;j<guessAsString.length;j++){
-                let g = guessAsString.charAt(j);
-                if (s===g){
-                    if (i===j){
-                        perfectMatch++;
-                    }  else {
-                        partialMatch++;
-                    }
-                }
-            }
-        }
-        let evalstr = ""
-        if (perfectMatch === 0 && partialMatch === 0){
-            evalstr = "No match";
-        } else {
-            if (partialMatch>0)
-                evalstr = `-${partialMatch}`;
-            if (perfectMatch>0)
-                evalstr += `+${perfectMatch}`;
-        }
-        return {perfectMatch, partialMatch, evalstr}
-    }
-
-    play = () => {
-        let tries = this.state.tries;
-        let moves = [...this.state.moves];
-        let gameLevel = this.state.gameLevel;
-        let stats = {...this.state.statistics}
-        tries++;
-        if (this.state.secret === this.state.guess){
-            gameLevel++;
-            if (gameLevel>10){
-                stats.wins++;
-                this.setState({
-                    tries: 0,
-                    moves: [],
-                    gameLevel : 3,
-                    secret: this.createSecret(3),
-                    statistics: stats
-                }, () => {
-                    let state = {...this.state}
-                    localStorage.setItem("mastermind-state", JSON.stringify(state))
-                })
-                this.props.history.push("/wins");
-            } else {
-                this.setState({
-                    tries: 0,
-                    moves: [],
-                    gameLevel : gameLevel,
-                    secret: this.createSecret(gameLevel)
-                }, () => {
-                    let state = {...this.state}
-                    localStorage.setItem("mastermind-state", JSON.stringify(state))
-                })
-            }
-        } else {
-            if (tries > 3){
-                stats.loses++;
-                this.setState({
-                    tries: 0,
-                    moves: [],
-                    secret: this.createSecret(gameLevel),
-                    statistics: stats
-                }, () => {
-                    let state = {...this.state}
-                    localStorage.setItem("mastermind-state", JSON.stringify(state))
-                })
-                this.props.history.push("/loses");
-            } else {
-                let evaluation = this.evaluateMove(this.state.secret, this.state.guess);
-                moves.push(new Move(this.state.guess, evaluation));
-                this.setState({ // async
-                    tries: tries,
-                    moves: moves
-                }, () => {
-                    let state = {...this.state}
-                    localStorage.setItem("mastermind-state", JSON.stringify(state))
-                })
-            }
-        }
-    }
-    render = () => {
         let tableMoves = "";
-        if (this.state.moves.length > 0){
+        if (props.game.moves.length > 0){
             tableMoves = <table className="table table-striped table-hover table-responsive table-bordered">
                 <thead>
                 <tr>
@@ -150,7 +30,7 @@ export default class Mastermind extends React.PureComponent {
                 </tr>
                 </thead>
                 <tbody>{
-                    this.state.moves.map( move =>
+                    props.game.moves.map( move =>
                         <tr key={move.guess}>
                             <td>{move.guess}</td>
                             <td>{move.evaluation.evalstr}</td>
@@ -160,29 +40,29 @@ export default class Mastermind extends React.PureComponent {
             </table>;
         }
         let badgeTries = "";
-        if (this.state.tries>0){
-            badgeTries = <Badge label="Tries" value={this.state.tries} />
+        if (props.game.tries>0){
+            badgeTries = <Badge label="Tries" value={props.game.tries} />
         }
-        return(
+        return (
           <div className="container">
               <div className="card">
                   <div className="card-header">
                     <CardTitle title="Game Console" />
                   </div>
                   <div className="card-body">
-                      <Badge label="Game Level" value={this.state.gameLevel} />
+                      <Badge label="Game Level" value={props.game.gameLevel} />
                       {badgeTries}
-                      <Badge label="Secret" value={this.state.secret} />
+                      <Badge label="Secret" value={props.game.secret} />
                       <div className="form-group">
                           <label htmlFor="guess">Guess: </label>
                           <input type="number"
                                  id="guess"
-                                 value={this.state.guess}
-                                 onChange={this.handleInputGuess}
+                                 value={props.game.guess}
+                                 onChange={props.handleInputGuess}
                                  className="form-control"></input>
                       </div>
                       <div className="form-group">
-                          <button  onClick={this.play}
+                          <button  onClick={props.play}
                                    className="btn btn-success">Play</button>
                       </div>
                       <p></p>
@@ -192,8 +72,7 @@ export default class Mastermind extends React.PureComponent {
                   </div>
               </div>
               <p></p>
-              <GameStatistics stats={this.state.statistics} />
+              <GameStatistics stats={props.game.statistics} />
           </div>
         );
-    }
 }
